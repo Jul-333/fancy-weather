085c388fa29914688c39e40ada706da867ac875b
Jul-333 <potapchuk_potapchuk_jul1996@list.ru>
2021-06-03 15:31:44 +0300

    fix: delete proxy

--- src/models/API/getWeatherForecast.js
+++ src/models/API/getWeatherForecast.js
@@ -1,8 +1,8 @@
-import { keyDarksky, proxy } from '../utils/config';
+import { keyDarksky } from '../utils/config';
 
 export async function getWeatherForecast(locationCoordinates) {
   return fetch(
-    `${proxy}https://api.darksky.net/forecast/${keyDarksky}/${locationCoordinates}?units=auto`
+    `https://api.darksky.net/forecast/${keyDarksky}/${locationCoordinates}?units=auto`
   )
     .then((res) => res.json())
     .then((data) => data)
